3b8ebac906052e21056cd7367e914abb395ddb7d
Pablo Hernández Nonide <pablo.hernandez@gigigo.com>
2018-10-15 10:58:36 +0200

    Update: Create /src and config now.sh

--- app.js
+++ app.js
@@ -3,21 +3,21 @@ const express = require('express')
 const createError = require('http-errors')
 const cors = require('cors')
 
-const app = express();
+const app = express()
 
-const config = require('./lib/utils').getConfig()
-const indexRouter = require('./routes/index')
+const config = require('./src/lib/utils').getConfig()
+const indexRouter = require('./src/routes/index')
 
 app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.use('/', indexRouter);
+app.use('/', indexRouter)
 
 app.use(function(req, res, next) {
   next(createError(404))
-});
+})
 
 app.use(function(err, req, res, next) {
   res.status(err.status || 500)
@@ -25,4 +25,4 @@ app.use(function(err, req, res, next) {
 
 app.listen(config.port)
 
-module.exports = app;
+module.exports = app
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,7 +1,7 @@
 const yaml = require('js-yaml')
 const fs = require('fs')
 const path = require('path')
-const configPath = path.join(__dirname, '../config.yml')
+const configPath = path.join(__dirname, '../../config.yml')
 
 module.exports.getConfig = function() {
   return yaml.safeLoad(fs.readFileSync(configPath, 'utf8'))
